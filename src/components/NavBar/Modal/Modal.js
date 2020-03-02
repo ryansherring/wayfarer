3830fca80288c30f1e3c600381c0ce10fc847a2d
Ryan <ryansherring@gmail.com>
2020-03-02 15:08:09 -0800

    makes login and signup modals

---
+++ src/components/NavBar/Modal/Modal.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import { Button, Modal, Form, } from 'semantic-ui-react';
+import axios from 'axios'
+import { render } from '@testing-library/react';
+
+
+
+class Register extends Component {
+    state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: ''
+    };
+  
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }; // makes the form fillable
+
+      handleSubmit = event => {
+        event.preventDefault()
+        console.log(this.state)
+        axios.post(`${process.env.REACT_APP_API_URL}/register`, this.state)
+        .then(res => {
+          console.log(res);
+        this.props.history.push('/login');
+        }).catch(err => {
+          console.log(err.response);
+        }); // on submit
+      };
+
+      render(){
+        return(
+            <Modal trigger={<Button>Sign Up</Button>}>
+            <Modal.Header>Register!</Modal.Header>
+            <Modal.Content Form>
+              <Modal.Description>
+                <p>Sign Up!</p>
+              </Modal.Description>
+              <div className='container mt-4'>
+                <div className='row'>
+                  <div className='col-md-4 offset-md-4'>
+                    <h4 className='mb-3'>Register</h4>
+                     <Form onSubmit={this.handleSubmit}>
+                      <Form.Field>
+                          <label htmlFor='name'>Name</label>
+                          <input
+                            onChange={this.handleChange}
+                            className='form-control form-control-lg'
+                            type='text'
+                            id='name'
+                            name='name'
+                            value={this.state.name}
+                          />
+                        </Form.Field>
+                        <Form.Field>
+                          <label htmlFor='name'>Email</label>
+                          <input
+                            onChange={this.handleChange}
+                            className='form-control form-control-lg'
+                            type='email'
+                            id='email'
+                            name='email'
+                            value={this.state.email}
+                          />
+                        </Form.Field>
+                        <Form.Field>
+                          <label htmlFor='name'>Password</label>
+                          <input
+                            onChange={this.handleChange}
+                            className='form-control form-control-lg'
+                            type='password'
+                            id='password'
+                            name='password'
+                            value={this.state.password}
+                          />
+                        </Form.Field>
+                        <Form.Field>
+                          <label htmlFor='password2'>Confirm Password</label>
+                          <input
+                            onChange={this.handleChange}
+                            className='form-control form-control-lg'
+                            type='password'
+                            id='password2'
+                            name='password2'
+                            value={this.state.password2}
+                          />
+                        </Form.Field>
+                      </Form> 
+                    </div>
+                  </div>
+                </div>
+            </Modal.Content>
+            <Modal.Actions>
+              <button className='btn btn-primary float-right' onClick={this.handleSubmit}>
+                  Register
+              </button>
+            </Modal.Actions>
+          </Modal>
+        );
+        }
+    }
+
+    
+
+    
+export default Register;
