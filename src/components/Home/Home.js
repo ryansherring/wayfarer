810c6b80746724dfe65a954f72e2ed05e0aa471c
jason-toups <me@jasontoups.com>
2020-03-02 12:33:47 -0800

    adds carousel to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import Carousel from 'semantic-ui-carousel-react';
+import { Image, Container } from 'semantic-ui-react';
+import './Home.css'
+import neworleans from './new-orleans.jpg';
+import paris from './paris.jpg';
 
-const Home = () => (
-  <div className="container">
-    <h1>Homepage goes here</h1>
-  </div>
-);
+const Home = () => {
+  let elements = [
+    {
+      render: () => {
+        return <Image
+          src={neworleans}
+          // size='large'
+          style={{ maxHeight: '400px' }}
+          centered
+        ></Image>
+      }
+    },
+    {
+      render: () => {
+        return <Image
+          src={paris}
+          // size='large'
+          style={{ maxHeight: '400px' }}
+          centered
+        ></Image>
+      }
+    },
+  ]
 
+  // const styles = {
+  //   height: '900px'
+  // }
+  return (
+    <Container
+    // style={styles}
+    >
+      <Carousel
+
+        elements={elements}
+        duration={3000}
+        animation='slide left'
+        showNextPrev={false}
+        showIndicators={true}
+      />
+    </Container>
+  )
+
+}
 export default Home;
