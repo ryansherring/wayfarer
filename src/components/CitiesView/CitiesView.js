a00c555669c549b6ef942cc2bdb152209f206263
jason-toups <me@jasontoups.com>
2020-03-03 12:27:45 -0800

    adds citiesview page with two columns

---
+++ src/components/CitiesView/CitiesView.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Container, Header, Grid } from 'semantic-ui-react';
+
+const CitiesView = () => {
+  return (
+    <Grid columns={2}>
+      <Grid.Column width={6} color='red'>
+        <Grid.Row>
+          <Header as='h1' inverted>Cities List Computational Component</Header>
+          {/* future space for Cities List Computational Component */}
+        </Grid.Row>
+      </Grid.Column>
+      <Grid.Column width={10} color='purple'>
+        <Grid.Row>
+          <Header as='h1' inverted>City Detail Container</Header>
+          {/* future space for City Detail Container */}
+        </Grid.Row>
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default CitiesView
--- src/config/routes.js
+++ src/config/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import Register from '../components/NavBar/Modal/ModalRegister';
 // import Login from '../components/NavBar/Modal/ModalLogin';
+import CitiesView from '../components/CitiesView/CitiesView'
 
-
+//TODO add a currentUser validation to CitiesView
 export default ({ currentUser, setCurrentUser }) => (
   <Switch>
     <Route exact path='/' component={Home} />
+    <Route path='/cities' component={CitiesView} />
     {/* <Route
       path='/login'
       render={() => (
