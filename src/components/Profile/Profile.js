e92bf64affb9f191ae173392c24dcaec4354e076
jason-toups <me@jasontoups.com>
2020-03-04 13:11:17 -0800

    updates profile styling with semantic

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Container, Segment } from 'semantic-ui-react';
 
 const Profile = (props) => {
-	const date = Date(props.profile.createdAt);
+  const date = Date(props.profile.createdAt);
   return (
-    <div>
+    <Segment inverted>
       <h1>{props.profile.name && props.profile.name.split(' ')[0]}'s Profile</h1>
-      <p><strong>Email:</strong> {props.profile.email}</p>
-      <p><strong>Home City:</strong> {props.profile.homeCity}</p>
-      <p><strong>Join Date:</strong> {date}</p>
-    </div>
+      <p><strong>Email:</strong></p>
+      <p>{props.profile.email}</p>
+      <p><strong>Home City:</strong></p>
+      <p>{props.profile.homeCity}</p>
+      <p><strong>Join Date:</strong></p>
+      <p>{date}</p>
+    </Segment>
   )
 };
 
