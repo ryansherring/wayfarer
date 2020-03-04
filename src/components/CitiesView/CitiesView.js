b33240684ab0769bb4a2425806e4a1481b5982e0
jason-toups <me@jasontoups.com>
2020-03-04 09:06:16 -0800

    updates cityview to restrict height

--- src/components/CitiesView/CitiesView.js
+++ src/components/CitiesView/CitiesView.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import { Container, Header, Grid, GridColumn } from 'semantic-ui-react';
 import PostContainer from '../PostContainer/PostContainer';
 import CitiesList from './CitiesList/CitiesList'
+import './CitiesView.css'
 
 const CitiesView = () => {
   return (
-    <Grid columns={2}>
-      <Grid.Column width={6}>
-        <Grid.Row>
-          <Header as='h1' inverted>Cities List</Header>
-          <CitiesList />
-        </Grid.Row>
-      </Grid.Column>
-      <Grid.Column width={10}>
-        <Grid.Row>
-          <PostContainer />
-        </Grid.Row>
-      </Grid.Column>
-    </Grid>
+    <div className='cities-view'>
+      <Grid columns={2}>
+        <Grid.Column width={6}>
+          <Grid.Row>
+            <Header as='h1' inverted>Cities List</Header>
+            <CitiesList />
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Grid.Row>
+            <PostContainer />
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
+    </div>
   )
 }
 
