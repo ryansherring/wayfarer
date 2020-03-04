a58eac5494fa80611064af5481bc53616a729f39
jason-toups <me@jasontoups.com>
2020-03-03 22:13:27 -0800

    CitiesList container complete, and updates images and links to images

--- src/components/CitiesView/CitiesView.js
+++ src/components/CitiesView/CitiesView.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Container, Header, Grid, GridColumn } from 'semantic-ui-react';
 import PostContainer from '../PostContainer/PostContainer';
+import CitiesList from './CitiesList/CitiesList'
 
 const CitiesView = () => {
   return (
     <Grid columns={2}>
-      <Grid.Column width={6} color='red'>
+      <Grid.Column width={6}>
         <Grid.Row>
-          <Header as='h1' inverted>Cities List Computational Component</Header>
-          {/* future space for Cities List Computational Component */}
+          <Header as='h1' inverted>Cities List</Header>
+          <CitiesList />
         </Grid.Row>
       </Grid.Column>
       <Grid.Column width={10}>
