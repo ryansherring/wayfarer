3087452c824165087bd1d55e84cc4137b38e2213
ksrawr <kennethsurban62@gmail.com>
2020-03-04 11:21:28 -0800

    updates profile info to display city and join date

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Profile = (props) => {
-
+	const date = Date(props.profile.createdAt);
   return (
     <div>
       <h1>{props.profile.name && props.profile.name.split(' ')[0]}'s Profile</h1>
       <p><strong>Email:</strong> {props.profile.email}</p>
+      <p><strong>Home City:</strong> {props.profile.homeCity}</p>
+      <p><strong>Join Date:</strong> {date}</p>
     </div>
   )
 };
