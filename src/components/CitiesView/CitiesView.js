08474df7bf3da0c48dc50edd93e3c96e424da33d
rerueter <rerueter@gmail.com>
2020-03-04 11:50:53 -0800

    updates some post detail styling

--- src/components/CitiesView/CitiesView.js
+++ src/components/CitiesView/CitiesView.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import { Container, Header, Grid, GridColumn } from 'semantic-ui-react';
-import PostContainer from '../PostContainer/PostContainer';
-import CitiesList from './CitiesList/CitiesList'
+import React, { Component } from "react";
+import { Container, Header, Grid, GridColumn } from "semantic-ui-react";
+import PostContainer from "../PostContainer/PostContainer";
+import CitiesList from "./CitiesList/CitiesList";
+import "./CitiesView.css";
 
 const CitiesView = () => {
   return (
-    <Grid columns={2}>
+    <Grid id="cities-view" columns={2}>
       <Grid.Column width={6}>
         <Grid.Row>
-          <Header as='h1' inverted>Cities List</Header>
+          <Header as="h1" inverted>
+            Cities List
+          </Header>
           <CitiesList />
         </Grid.Row>
       </Grid.Column>
@@ -18,7 +21,7 @@ const CitiesView = () => {
         </Grid.Row>
       </Grid.Column>
     </Grid>
-  )
-}
+  );
+};
 
-export default CitiesView
+export default CitiesView;
