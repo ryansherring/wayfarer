0f06642251477051bae742fdd75c0aca8844b2fb
rerueter <rerueter@gmail.com>
2020-03-03 22:15:27 -0800

    super updates post detail functionalityand styling

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -6,15 +6,25 @@ const PostDetail = props => {
   console.log({ PDprops: props });
 
   const postToShow = API.filter(post => post._id === props.match.params.id);
+
   console.log(postToShow[0]._id);
+
   const { title, date, content, user, img } = postToShow[0];
+
   return (
-    <section className="post-detail">
-      <h1 onClick={props.history.goBack}>{title}</h1>
-      <img src={img} alt={title}></img>
-      <h2>{user}</h2>
-      <h2>{date}</h2>
-      <p>{content}</p>
+    <section className="aligner">
+      <article className="post-detail">
+        <h1 className="post-detail-header">{title}</h1>
+        <img className="post-detail-img" src={img} alt={title}></img>
+        <div className="post-detail-meta">
+          <h4>{user}</h4>
+          <h4>{date}</h4>
+        </div>
+        <p>{content}</p>
+        <div className="post-detail-return" onClick={props.history.goBack}>
+          <h4>Go Back</h4>
+        </div>
+      </article>
     </section>
   );
 };
