9f597ed3f997bc80d67e25c3a09fbc9e85e13aac
Ryan <ryansherring@gmail.com>
2020-03-04 12:03:32 -0800

    validates redirects if logged in

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
-import Register from "../components/NavBar/Modal/ModalRegister";
 import Login from "../components/NavBar/Modal/ModalLogin";
 import PostContainer from "../components/PostContainer/PostContainer";
 import ProfileContainer from "../components/ProfileContainer/ProfileContainer";
@@ -10,7 +9,7 @@ import PostDetail from "../components/PostDetail/PostDetail";
 
 export default ({ currentUser, setCurrentUser }) => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={currentUser ? CitiesView : Home} />
     <Route exact path="/cities" component={CitiesView} />
     <Route
       path="/login"
